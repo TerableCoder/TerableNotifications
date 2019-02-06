875fed82211c83cbe08d960a421c816bf1a52f1b
Tera Coder <TerableCoder@gmail.com>
2019-02-06 04:34:15 -0500

    Untested, but should work

---
+++ README.md
@@ -0,0 +1,24 @@
+# TerableNotifications
+Alerts you when you get /w ed, or when someone types @@ or @myName in chat.
+
+The default is to only notify you if you're AFK.
+
+## Usage
+### `teran w/whisper`
+- Toggle /w notify on/off, default is on
+### `teran wafk/whisperafk`
+- Toggle /w AFK notify on/off, default is on
+### `teran at/atat`
+- Toggle @@/@character.name notify on/off, default is on
+### `teran atafk/atatafk`
+- Toggle @@/@character.name AFK notify on/off, default is on
+
+## Conflicts
+Conflicts with the proxy module 'afk-notify'
+
+## Requires
+- [tera-notifier](https://github.com/SerenTera/tera-notifier)
+
+## Todo
+- Add notification options for broker requests
+- Recieve notifications only from certain channels of your choosing
---
+++ index.js
@@ -0,0 +1,62 @@
+module.exports = function TerableNotifications(mod) {
+	const notifier = mod.require.notifier;
+	const command = mod.command || mod.require.command;
+	let myName = null;
+	
+	if(mod.proxyAuthor !== 'caali'){
+		const options = require('./module').options
+		if(options){
+			const settingsVersion = options.settingsVersion
+			if(settingsVersion){
+				mod.settings = require('./' + (options.settingsMigrator || 'module_settings_migrator.js'))(mod.settings._version, settingsVersion, mod.settings)
+				mod.settings._version = settingsVersion
+			}
+		}
+	}
+	mod.hook('S_LOGIN', 12, (event) => {
+		myName = event.name;
+    });
+	mod.game.on('leave_game', () => {
+        myName = null;
+    });
+	
+	mod.hook('S_WHISPER', 2, (event) => { 
+		if(mod.game.me.is(event.player)) return;
+		if(mod.settings.whisper){
+			if(mod.settings.whisperafk) notifier.messageafk(`[${event.authorName}] ${event.message}`);
+			else notifier.message(`[${event.authorName}] ${event.message}`);
+		}
+		if(mod.settings.atat && (event.message.includes(`@@`) || (myName != null &&  event.message.includes(`@${myName}`)))){
+			if(mod.settings.atatafk) notifier.message(`[${event.authorName}] @@`);
+			else notifier.message(`[${event.authorName}] @@`);
+		}
+	});
+	mod.hook('S_CHAT', 2, (event) => {
+		if(mod.game.me.is(event.player)) return;
+		if(mod.settings.atat && (event.message.includes(`@@`) || (myName != null &&  event.message.includes(`@${myName}`)))){
+			if(mod.settings.atatafk) notifier.message(`[${event.authorName}] @@`);
+			else notifier.message(`[${event.authorName}] @@`);
+		}
+	});
+	mod.hook('S_PRIVATE_CHAT', 1, (event) => {
+		if(mod.game.me.is(event.player)) return;
+		if(mod.settings.atat && (event.message.includes(`@@`) || (myName != null &&  event.message.includes(`@${myName}`)))){
+			if(mod.settings.atatafk) notifier.message(`[${event.authorName}] @@`);
+			else notifier.message(`[${event.authorName}] @@`);
+		}
+	});
+	
+	command.add(['teran', 'tnotify', 'terablenotifications'], (p1) => {
+		if(p1){
+			p1 = p1.toLowerCase();
+			if(p1 == "w" || "whisper") mod.settings.whisper = !mod.settings.whisper;
+			if(p1 == "wafk" || "whisperafk") mod.settings.whisperafk = !mod.settings.whisperafk;
+			else if(p1 == "at" || "atat") mod.settings.atat = !mod.settings.atat;
+			else if(p1 == "atafk" || "atatafk") mod.settings.atatafk = !mod.settings.atatafk;
+			else command.message("Invalid argument. Type 'teran w', 'teran wafk', 'teran at', or 'teran atafk'");
+			mod.saveSettings();
+		} else command.message("Invalid argument. Type 'teran w', 'teran wafk', 'teran at', or 'teran atafk'");
+		command.message(`Whisper notify is ${mod.settings.whisper ? "enabled" : "disabled"} & Whisper afk is ${mod.settings.whisperafk ? "enabled" : "disabled"}.`);
+		command.message(`@@ notify is ${mod.settings.atat ? "enabled" : "disabled"} & @@ afk is ${mod.settings.atatafk ? "enabled" : "disabled"}.`);
+	});
+}
